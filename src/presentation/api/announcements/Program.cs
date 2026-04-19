83a7a9ac9b20f8c84343539cd2b5c07e9687e447
Emerson Trindade (menso.dev.br) <menso@menso.dev.br>
2026-04-18 23:01:35 -0300

    fix db and query filter
    Co-authored-by: Mikael Santos <mykaels007@gmail.com>

--- src/presentation/api/announcements/Program.cs
+++ src/presentation/api/announcements/Program.cs
@@ -1,27 +1,40 @@
 using Advent.Announcements.Api.Endpoints;
 using Advent.Announcements.Application;
 using Advent.Announcements.Infrastructure;
-//using Scalar.AspNetCore;
+using Advent.Announcements.Infrastructure.Contexts;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+// builder.Services.AddSwaggerGen();
+
 builder.Services.AddOpenApi();
 
 builder.Services.AddAnnouncementsApplicationServices(builder.Configuration);
 builder.Services.AddAnnouncementsInfrastructureServices(builder.Configuration);
 
+
 var app = builder.Build();
+
+#if DEBUG
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AnnouncementDbContext>();
+    context.Database.EnsureCreated();
+}
+
+#endif
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    //app.MapScalarApiReference();
+    // app.UseSwagger();
+    // app.UseSwaggerUI();
+    app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
--- src/services/announcements/infrastructure/AnnouncementsServices.cs
+++ src/services/announcements/infrastructure/AnnouncementsServices.cs
@@ -10,19 +10,19 @@ namespace Advent.Announcements.Infrastructure;
 public static class AnnouncementsServices
 {
     private const string DbConnectionStringName = "DefaultConnection";
-    
+
     public static IServiceCollection AddAnnouncementsInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
         // Db
         services.AddDbContext<AnnouncementDbContext>(option =>
             option.UseSqlServer(configuration.GetConnectionString(DbConnectionStringName))
         );
-        
+
         services.AddScoped<IAnnouncementUnitOfWork>(sp => sp.GetRequiredService<AnnouncementDbContext>());
-        
+
         // Repositories
         services.AddScoped<INoticeRepository, NoticeRepository>();
-        
+
         return services;
     }
 }
